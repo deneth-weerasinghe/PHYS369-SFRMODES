5de9dab6f1adb43a2cdc5f05f5050b9aa8032f5b
Deneth <deneth@hotmail.co.uk>
2023-02-20 12:03:02 +0000

    Test on MacOS

--- Plots/plotting_code/sfr_divergence.py
+++ Plots/plotting_code/sfr_divergence.py
@@ -3,11 +3,11 @@ import matplotlib.pyplot as plt
 import csv
 import os
 
-path = os.path.dirname(os.path.dirname(os.getcwd()))  # obtains path of top-level directory
+path = os.getcwd()  # obtains path of top-level directory
 
 
 def plot():
-    with open(path + "\data\csv\sample_data.csv", newline='') as csvfile:
+    with open(path + "/data/csv/sample_data.csv", newline='') as csvfile:
         reader = csv.reader(csvfile, quoting=csv.QUOTE_NONNUMERIC)
         first_row = next(reader)  # prints the first row, as reference for column names
         print(first_row)
@@ -30,10 +30,10 @@ def plot():
         error1 = np.array([sfr_med_MIN_ERR, sfr_med_MAX_ERR])
         # print(error1)
 
-        plt.rcParams['text.usetex'] = True
+        # plt.rcParams['text.usetex'] = True
         plt.errorbar(mass_med, sfr_med, yerr=error1, c="blue", fmt="o", label="SFR_MED")
         plt.errorbar(mass_med, sfr_best, c="red", fmt="o", label="SFR_BEST")
-        plt.title("Logarithmic Stellar Mass against SFR (median pdf) and SFR (minimum of $\chi^2$)")
+        plt.title("Logarithmic Stellar Mass against SFR (median pdf) and SFR (minimum of chi^2)")
         plt.xlabel("log MASS_MED")
         plt.ylabel("log SFR")
         plt.legend()
@@ -54,6 +54,7 @@ def plot_sfr(x, y):
     plt.xlabel("Median Stellar Mass")
     plt.ylabel("Stellar Formation Rate")
     plt.show()
-
-
+print('Hello WORLD')
+print(path)
+print('YOOOOOOOOOOOOOOOOOOO')
 plot()
