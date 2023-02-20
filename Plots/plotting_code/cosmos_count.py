3620575d3d4c777bf8918f9b84d01ff82169d62f
Deneth <deneth@hotmail.co.uk>
2023-02-20 12:18:43 +0000

    Calculated count of COSMOS objects in full_table

---
+++ Plots/plotting_code/cosmos_count.py
@@ -0,0 +1,16 @@
+import csv
+import os
+
+path = os.getcwd()  # obtains path of top-level directory
+
+def cosmos_count():
+    with open(path + "/data/csv/full_table.csv", newline='') as csvfile:
+        reader = csv.reader(csvfile)
+        next(reader)
+        count = 0
+        for row in reader:
+            if (148.0 < float(row[1]) < 152.0) and (1.5 < float(row[2]) < 3.5):  # checks if coords match range of COSMOS survey
+                count += 1
+        print(count)
+
+cosmos_count()
