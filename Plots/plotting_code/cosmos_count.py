ea9a28f91c76813b997b0ed1c3a1b4c910bff194
Deneth <deneth@hotmail.co.uk>
2023-02-21 20:01:45 +0000

    Extracted all COSMOS objects from our dataset

--- Plots/plotting_code/cosmos_count.py
+++ Plots/plotting_code/cosmos_count.py
@@ -4,13 +4,18 @@ import os
 path = os.getcwd()  # obtains path of top-level directory
 
 def cosmos_count():
-    with open(path + "/data/csv/full_table.csv", newline='') as csvfile:
-        reader = csv.reader(csvfile)
-        next(reader)
+    with open(path + "/data/csv/group_full_table.csv", newline='') as r, open(path + "/data/csv/group_COSMOS.csv", 'w', newline='') as w:
+        reader = csv.reader(r)
+        writer = csv.writer(w)
+        
+        writer.writerow(next(reader))  # writes the heading row
+        
         count = 0
+
         for row in reader:
             if (148.0 < float(row[1]) < 152.0) and (1.3 < float(row[2]) < 3.5):  # checks if coords match range of COSMOS survey
                 count += 1
+                writer.writerow(row)
         print(count)
 
 cosmos_count()
