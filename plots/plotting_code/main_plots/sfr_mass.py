dce6ff69fcdddde768c1244ca5a6e1d82fd2a48c
Deneth <deneth@hotmail.co.uk>
2023-02-27 12:01:28 +0000

    Star formation against mass script first attempt

--- plots/plotting_code/main_plots/sfr_mass.py
+++ plots/plotting_code/main_plots/sfr_mass.py
@@ -8,26 +8,55 @@ def data_gather(sample_size=100):
 
     data = []
 
-    with open(path + '/data/csv/COSMOS_survey/COSMOS_0_OUT.csv', newline='') as csvfile:
-        reader = csv.reader(csvfile, quoting=csv.QUOTE_NONNUMERIC)
+    for j in range(0, 2):
+        with open(path + f'/data/csv/COSMOS_survey/COSMOS_{j}_OUT.csv', newline='') as csvfile:
+            reader = csv.reader(csvfile, quoting=csv.QUOTE_NONNUMERIC)
 
-        next(reader)
-        '''
-        Columns extracted:
+            next(reader)
+            '''
+            Columns extracted:
 
-        Index | Column Name | Notes
-        2 | type | 0 = galaxy, 1 = star, 2 = X-ray source
-        3 | zpdf | redshift, mean value
-        4 | zpdf_l68 | z lower limit, 68% confidence
-        5 | zpdf_h68 | z upper limit, 68% confidence
-        6 | zminchi2 | z value from chi^2
-        7 | chi2_best | reduced chi^2 for zminchi^2
-        8 | m_i | absolute magnitude in i band
-
-        '''
-        for i, row in enumerate(reader):
-            if i == sample_size:
-                break
-            if row[2] == 0:  # checks if the source is a galaxy
-                data.append([row[3], row[4], row[5], row[6], row[7], row[8]])
+            Index | Column Name | Notes
+            2 | type | 0 = galaxy, 1 = star, 2 = X-ray source
+            11 | mass_med | stellar mass from the pdf
+            12 | mass_med_min68 | stellar mass lower limit, 68% confidence
+            13 | mass_med_max68 | stellar mass upper limit, 68% confidence
+            14 | mass_best | stellar mass from chi^2
+            15 | sfr_med | star formation rate from the pdf
+            16 | sfr_med_min68 | star formation rate lower limit, 68% confidence 
+            17 | sfr_med__max68 | star formation rate upper limit, 68% confidence
+            18 | sfr_best | star formation rate from chi^2
+            '''
+            for i, row in enumerate(reader):
+                if i == sample_size:
+                    break
+                if row[2] == 0:  # checks if the source is a galaxy
+                    data.append([row[11], row[12], row[13], row[14], row[15], row[16], row[17], row[18]])
     return data
+
+
+
+
+
+def generate_plot(data):
+    mass_med = [i[0] for i in data]
+    mass_low_err = [i[0] - i[1] for i in data]
+    mass_upp_err = [i[2] - i[0] for i in data]
+    sfr_med = [i[4] for i in data]
+    sfr_low_err = [i[4] - i[5] for i in data]
+    sfr_upp_err = [i[6] - i[4] for i in data]
+
+    mass_err = [mass_low_err, mass_upp_err]
+    sfr_err = [sfr_low_err, sfr_upp_err]
+
+    plt.errorbar(mass_med, sfr_med, xerr=mass_err, yerr=sfr_err, fmt='o', c='red', ecolor='black', label='plot1')
+    plt.title('Star formation rates in COSMOS mergers against stellar mass')
+    plt.xlabel('log Stellar Mass')
+    plt.ylabel('log Star Formation Rate')
+    plt.show()
+
+
+mydata = data_gather(sample_size=1000)
+
+generate_plot(mydata)
+
--- plots/plotting_code/match_COSMOS.py
+++ plots/plotting_code/match_COSMOS.py
@@ -9,14 +9,14 @@ path = os.getcwd()  # obtains path of top-level directory
 
 def match_COSMOS(i):
     myrows = []
-    with open(path + '/data/csv/group_COSMOS.csv', newline='') as f1, open(path + f'/data/csv/COSMOS_survey/COSMOS_{i}.csv') as f2:
+    with open(path + '/data/csv/group_COSMOS.csv', newline='') as f1, open(path + f'/data/csv/COSMOS_survey/COSMOS_{i}_OUT.csv') as f2:
         group_data = csv.reader(f1)
         cosmos_data = csv.reader(f2)
         next(group_data)  
         next(cosmos_data)  # skips the headers
 
         offset = 0.00001
-        tol = 0.0001
+        tol = 0.001
 
         for group_row in group_data:
             for cosmos_row in cosmos_data:
@@ -29,16 +29,17 @@ def match_COSMOS(i):
                 # if (ra_lower < float(row1[1]) < ra_upper) and (dec_lower < float(row1[2]) < dec_upper):
                     # myrows.append(row2)
                 
-                ra_diff = abs(float(group_row[1])-float(cosmos_row[0]))#abs(150.67986638-150.679866)
-                ra_tol = tol*max(float(group_row[1]), float(cosmos_row[0]))#tol*max(150.67986638, 150.679866)
-                dec_diff = abs(float(group_row[2])-float(cosmos_row[1]))#abs(2.1966146-2.196563)
-                dec_tol = tol*max(float(group_row[2]), float(cosmos_row[1]))#tol*max(2.1966146, 2.196563)
+                ra_diff = abs(float(group_row[2])-float(cosmos_row[0]))#abs(150.67986638-150.679866)
+                ra_tol = tol*max(float(group_row[2]), float(cosmos_row[0]))#tol*max(150.67986638, 150.679866)
+                dec_diff = abs(float(group_row[3])-float(cosmos_row[1]))#abs(2.1966146-2.196563)
+                dec_tol = tol*max(float(group_row[3]), float(cosmos_row[1]))#tol*max(2.1966146, 2.196563)
                 
                 isRaMatching = ra_diff < ra_tol
                 isDecMatching = dec_diff < dec_tol
                 if isRaMatching and isDecMatching:
                     myrows.append(cosmos_row)
                     break
+                    
     return myrows
 
 
